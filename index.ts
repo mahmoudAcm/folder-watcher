d166d56308f48a98e17c2a7d1c47890f142d3955
mahmoudAcm <mahmoud.acm.123@gamil.com>
2022-03-11 15:05:13 +0200

    fix(outDir option)

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(addWatcherRouter);
 app.use(removeWatcherRouter);
 
